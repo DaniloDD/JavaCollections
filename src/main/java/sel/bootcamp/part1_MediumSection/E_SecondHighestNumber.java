2e36416b270d638077b77f412549ba0ec0f12040
Danilo Deiana <danilo.deiana0@gmail.com>
2024-08-01 16:33:25 -0400

    update

--- src/main/java/sel/bootcamp/part1_MediumSection/E_SecondHighestNumber.java
+++ src/main/java/sel/bootcamp/part1_MediumSection/E_SecondHighestNumber.java
@@ -1,41 +1,34 @@
 package sel.bootcamp.part1_MediumSection;
 
 public class E_SecondHighestNumber {
-	
-	public static void main (String [] args) {
-	
-		int[] numbers = {1, 10, 2, 9, 3, 8};
-		if (numbers.length <2) {
-			System.out.println("Array must contains more elements.");
-			return;
-		}
-		//int secondHighest = findSecondHighest(numbers);
-		//System.out.println("The second highest number is: " + secondHighest);
-	}
-	/*
-	public static int findSecondHighest(int[] num) {
-		
-		int highest = Integer.MIN_VALUE;
+
+	public static int findSecondHighest(int[] numbers) {
+		// Initialize the first and second highest to minimum integer values
+		int firstHighest = Integer.MIN_VALUE;
 		int secondHighest = Integer.MIN_VALUE;
-		
-		// for each number inside the array received in the parameters
-		for (int number: num) {
-			if (number > highest) {
-				secondHighest = highest;
-				highest = number;
-			}
-			else if(number > secondHighest && number != highest) {
+
+		for (int number : numbers) {
+			if (number > firstHighest) {
+				// Update second highest before changing first highest
+				secondHighest = firstHighest;
+				firstHighest = number;
+			} else if (number > secondHighest && number < firstHighest) {
+				// Update second highest if number is between first highest and second highest
 				secondHighest = number;
 			}
 		}
-		
+
+		return secondHighest;
+	}
+
+	public static void main(String[] args) {
+		int[] numbers = { 10, 5, 20, 8, 25, 20, 25 };
+		int secondHighest = findSecondHighest(numbers);
+
 		if (secondHighest == Integer.MIN_VALUE) {
-			System.out.println("There is no second highest num in the array");
-		}
-		else {
+			System.out.println("There is no second highest number.");
+		} else {
 			System.out.println("The second highest number is: " + secondHighest);
 		}
-		
 	}
-	*/
 }
